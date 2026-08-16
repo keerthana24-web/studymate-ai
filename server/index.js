e73b02ff6a7c4136555f1b637b1554101dbcc628
keerthana24-web <mkeerthana4128@gmail.com>
2026-08-16 18:41:20 +0530

    Prepare backend for deployment

--- server/index.js
+++ server/index.js
@@ -253,7 +253,7 @@ CONTENT REQUIREMENTS:
 // START SERVER
 // -------------------------------------
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(
